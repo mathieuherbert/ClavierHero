8f0c5209111e30531b877aec335f67eed2507741
mcartaud <mathieu.cartaud@etudiant-mines.nantes.fr>
2013-10-29 10:00:09 +0100

    ajout du debut de l'ecrand e fin

---
+++ Applications/KeyboardHero/ihm/AffichageFin.h
@@ -0,0 +1,19 @@
+/*
+ * AffichageFin.h
+ *
+ *  Created on: 29 oct. 2013
+ *      Author: mathieu
+ */
+
+#ifndef AFFICHAGEFIN_H_
+#define AFFICHAGEFIN_H_
+
+#include "drivers/ClavierV.h"
+
+static class AffichageFin {
+public:
+	static void afficherBonneFin(EcranV* e);
+	static void afficherMauvaiseFin(EcranV* e);
+};
+
+#endif /* AFFICHAGEFIN_H_ */
